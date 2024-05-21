1f9f13ef954552637fcf56702817d714f2e5120e
David Zamora <d.zamorac@uniandes.edu.co>
2024-05-21 11:01:27 -0500

    habilitacion de CORS

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'api/v',
     defaultVersion: '1',
   });
-
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
+  console.log('Servidor corriendo en el puerto 3000');
 }
+
 bootstrap();
