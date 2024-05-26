f9231c23a7721231906804db206c20b86d5bda45
David Zamora <d.zamorac@uniandes.edu.co>
2024-05-25 20:23:47 -0500

    gestor conectado, cambio de servicio para que devuevla ids

--- src/gestor-reporte/gestor-reporte.service.ts
+++ src/gestor-reporte/gestor-reporte.service.ts
@@ -59,7 +59,7 @@ export class GestorReporteService {
         return await this.gestorRepository.save(gestor);
     }
 
-    async getReportesGestor(idGestor: string): Promise<ReporteEntity[]> {
+    async getReportesGestor(idGestor: string): Promise<{ id: string }[]> {
 
         if (!/^[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}$/.test(idGestor)) {
             throw new BusinessLogicException("Invalid id format. HINT: Valid UUID values are of the form \'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF\'", BusinessError.BAD_REQUEST);
@@ -70,7 +70,7 @@ export class GestorReporteService {
             throw new BusinessLogicException("The manager with the given id was not found", BusinessError.NOT_FOUND);
         }
 
-        return gestor.reportes;
+        return gestor.reportes.map(reporte => ({ id: reporte.id }));
     }
 
     async getGestorReporte(idGestor: string, idReporte: string): Promise<ReporteEntity> {
